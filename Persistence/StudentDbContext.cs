e7a7e670f2d11bd84f87c856fea0821b5780a425
ashley_la_nena04 <ashley_la_nena04@hotmail.com>
2019-06-19 10:27:22 -0400

    Primer commit para cargar los archivos

---
+++ Persistence/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class StudentDbContext: DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+        public StudentDbContext(DbContextOptions<StudentDbContext> options)
+            : base(options)
+        { }
+    }
+}
